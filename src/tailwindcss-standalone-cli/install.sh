68cf5947e5bcf55d19b2a57b647343d41c1e44e6
r3dpoint <3207964+r3dpoint@users.noreply.github.com>
2023-02-08 16:02:33 +0000

    updates

--- src/tailwindcss-standalone-cli/install.sh
+++ src/tailwindcss-standalone-cli/install.sh
@@ -33,15 +33,15 @@ if [ "${architecture}" != "x86_64" ]; then
     exit 1
 fi
 
-apt_get_update() {
-    echo "Running apt-get update..."
-    apt-get update -y
-}
-
 # Checks if packages are installed and installs them if not
 check_packages() {
-    apt_get_update
-    apt-get -y install --no-install-recommends "$@"
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt-get update..."
+            apt-get update -y
+        fi
+        apt-get -y install --no-install-recommends "$@"
+    fi
 }
 
 export DEBIAN_FRONTEND=noninteractive
