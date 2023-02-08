019307f7616268a1a7008aa610fa9d12a7447087
Naresh Sikha <3207964+r3dpoint@users.noreply.github.com>
2023-02-07 21:58:52 -0800

    updates

--- src/tailwindcss-standalone-cli/install.sh
+++ src/tailwindcss-standalone-cli/install.sh
@@ -30,13 +30,7 @@ fi
 
 # Checks if packages are installed and installs them if not
 check_packages() {
-    if ! dpkg -s "$@" > /dev/null 2>&1; then
-        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-            echo "Running apt-get update..."
-            apt-get update -y
-        fi
-        apt-get -y install --no-install-recommends "$@"
-    fi
+    apt-get -y install --no-install-recommends "$@"
 }
 
 export DEBIAN_FRONTEND=noninteractive
