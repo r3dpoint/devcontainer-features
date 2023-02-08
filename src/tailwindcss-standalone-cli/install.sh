b211f4d5b21119628e50eea3dc9b2200c5357864
Naresh Sikha <3207964+r3dpoint@users.noreply.github.com>
2023-02-07 21:20:52 -0800

    updates

--- src/tailwindcss-standalone-cli/install.sh
+++ src/tailwindcss-standalone-cli/install.sh
@@ -6,6 +6,7 @@
 # - All devcontainers are Linux
 # Motivations
 # - https://github.com/devcontainers/features/blob/main/src/ruby/install.sh
+# - https://github.com/devcontainers/features/blob/main/src/aws-cli/install.sh
 
 version=${VERSION:-"latest"}
 if [ "${version}" != "latest" ]; then
@@ -24,6 +25,19 @@ if [ "${architecture}" != "x86_64" ]; then
     echo "(!) Architecture $architecture unsupported"
     exit 1
 fi
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            echo "Running apt-get update..."
+            apt-get update -y
+        fi
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+check_packages curl
 
 file="tailwindcss-linux-x64"
 curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/${version}/download/${file}
