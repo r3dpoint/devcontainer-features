49a5ec0858ae224d4e525b4b125ffdaf6cce21c1
r3dpoint <3207964+r3dpoint@users.noreply.github.com>
2023-02-08 18:24:58 +0000

    updates

--- src/tailwindcss-standalone-cli/NOTES.md
+++ src/tailwindcss-standalone-cli/NOTES.md
@@ -1,1 +1,3 @@
 # Notes
+
+Official TailwindCSS Standalone CLI Docs: <https://tailwindcss.com/blog/standalone-cli>
--- src/tailwindcss-standalone-cli/install.sh
+++ src/tailwindcss-standalone-cli/install.sh
@@ -27,13 +27,21 @@ if [ "${kernel}" != "Linux" ]; then
     exit 1
 fi
 
+# https://github.com/tailwindlabs/tailwindcss/releases
 architecture="$(uname -m)"
-if [ "${architecture}" != "x86_64" ]; then
+if [ "${architecture}" == "amd64" ]; then
+    file="tailwindcss-linux-x64"
+elif [ "${architecture}" == "x86_64" ]; then
+    file="tailwindcss-linux-x64"
+elif [ "${architecture}" == "arm64" ]; then
+    file="tailwindcss-linux-arm64"
+elif [ "${architecture}" == "aarch64" ]; then
+    file="tailwindcss-linux-arm64"
+else
     echo "(!) Architecture $architecture unsupported"
     exit 1
 fi
 
-file="tailwindcss-linux-x64"
 curl -sLO "https://github.com/tailwindlabs/tailwindcss/releases/${version}/download/${file}"
 chmod +x "${file}"
 mv "${file}" "/usr/local/bin/tailwindcss"
